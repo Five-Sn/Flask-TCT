3279158d664c2eb696c72e76b277398f2ee8a3a0
Five-Sn <32499540+Five-Sn@users.noreply.github.com>
2018-04-17 16:26:55 -0500

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -12,7 +12,79 @@ $('#B-full').click(function(){
         $('#list-full').show();
         list = '#list-full';
     } else {
+        $('#list-full').hide();var list = "none";
+/* All of the possible values for "list" and their corresponding IDs are:
+ * full     #list-full
+ * length     #list-length
+ */
+
+$('#list-full').hide();
+$('#list-length').hide();
+
+//Clicking the full list button:
+$('#B-full').click(function(){
+    if(list === '#list-full'){
+        list = "none";
+    } else {
+        list = '#list-full';
+    }
+    showHide();
+});
+
+//Clicking the length list button:
+$('#B-length').click(function(){
+    if(list === '#list-length'){
+        list = "none";
+    } else {
+        list = '#list-length';
+    }
+    showHide();
+});
+
+function showHide(){
+    if(list === '#list-full'){
+        $('#list-full').show();
+        $('#list-length').hide();
+    } else if (list === '#list-length'){
+        $('#list-length').show();
         $('#list-full').hide();
+    } else {
+        $('#list-length').hide();
+        $('#list-full').hide();
+    }
+}
+
+/*$('#B-full').click(function(){
+    if(list !== '#list-full'){
+        $('#list-full').show();
+        list = '#list-full';
+    } else {
+        $('#list-full').hide();
+        list = "none";
+    }
+});
+
+$('#B-length').click(function(){
+    if(list !== "length"){
+        //$('#list-length').show();
+        list = "length";
+    } else {
+        //$('#list-length').hide();
+        list = "none";
+    }
+});*/
+
+//I think this makes Popcode lag
+/*while(1 > 0){
+    if(list === null){
+        break;
+    }
+    if(list === '#list-full'){
+    $('#list-full').show();
+    } else {
+    $('#list-full').hide();
+    }
+}*/
         list = "none";
     }
 });
