9c8708d74412493c4a491618cf9ce64d6802a078
Five-Sn <32499540+Five-Sn@users.noreply.github.com>
2018-04-26 17:14:45 -0500

    Update home.js
    Connected database, removed old code

--- static/js/home.js
+++ static/js/home.js
@@ -1,112 +1,28 @@
-var list = "none";
-/* All of the possible values for "list" and their corresponding IDs are:
- * full     #list-full
- * length     #list-length
- */
+$(document).ready(function(){
+    console.log("I was correctly inited");
+    $("#search-submit").click(function(event) {
+        console.log("click event");
 
-$('#list-full').hide();
-$('#list-length').hide();
+        event.preventDefault();
 
-$('#B-full').click(function(){
-    if(list !== '#list-full'){
-        $('#list-full').show();
-        list = '#list-full';
-    } else {
-        $('#list-full').hide();var list = "none";
-/* All of the possible values for "list" and their corresponding IDs are:
- * full     #list-full
- * length     #list-length
- */
+        var trailLength = $("#length").val();
+        var trailName = $("#searchName").val();
+        var trailLocation = $("#location").val();
 
-$('#list-full').hide();
-$('#list-length').hide();
+        var queryString = "/trails" + "?name=" + trailName + "&length=" + trailLength + "&location=" + trailLocation;
+        console.log("query string:" + queryString);
 
-//Clicking the full list button:
-$('#B-full').click(function(){
-    if(list === '#list-full'){
-        list = "none";
-    } else {
-        list = '#list-full';
-    }
-    showHide();
+        $.getJSON( queryString, function (json) {
+            console.log(json);
+
+            var items = [];
+            items.push("<ul>");
+
+            $.each( json, function( key, val ) {
+                items.push( "<li>" + "Name: " + val.name + " | " + "Length: " + val.length + " | " + "Location: " + val.location + "</li>" );
+            });
+            items.push("</ul>");
+            $("#searchResults").html(items);
+        });
+    });
 });
-
-//Clicking the length list button:
-$('#B-length').click(function(){
-    if(list === '#list-length'){
-        list = "none";
-    } else {
-        list = '#list-length';
-    }
-    showHide();
-});
-
-function showHide(){
-    if(list === '#list-full'){
-        $('#list-full').show();
-        $('#list-length').hide();
-    } else if (list === '#list-length'){
-        $('#list-length').show();
-        $('#list-full').hide();
-    } else {
-        $('#list-length').hide();
-        $('#list-full').hide();
-    }
-}
-
-/*$('#B-full').click(function(){
-    if(list !== '#list-full'){
-        $('#list-full').show();
-        list = '#list-full';
-    } else {
-        $('#list-full').hide();
-        list = "none";
-    }
-});
-
-$('#B-length').click(function(){
-    if(list !== "length"){
-        //$('#list-length').show();
-        list = "length";
-    } else {
-        //$('#list-length').hide();
-        list = "none";
-    }
-});*/
-
-//I think this makes Popcode lag
-/*while(1 > 0){
-    if(list === null){
-        break;
-    }
-    if(list === '#list-full'){
-    $('#list-full').show();
-    } else {
-    $('#list-full').hide();
-    }
-}*/
-        list = "none";
-    }
-});
-
-$('#B-length').click(function(){
-    if(list !== "length"){
-        //$('#list-length').show();
-        list = "length";
-    } else {
-        //$('#list-length').hide();
-        list = "none";
-    }
-});
-
-//I think this makes Popcode lag
-/*while(1 > 0){
-    if(list === null){
-        break;
-    }
-    if(list === '#list-full'){
-    $('#list-full').show();
-    } else {
-    $('#list-full').hide();
-    }
-}*/
